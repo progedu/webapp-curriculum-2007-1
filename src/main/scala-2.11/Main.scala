af4733a1f5eb0ef8a149dd089685412498e247bc
murosan <masashi_16n1200013@nnn.ed.jp>
2017-03-29 15:54:56 +0900

    挑戦の初級

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,3 +1,6 @@
 object Main {
-  val tribs: Stream[Int] = ???
+  val tribs: Stream[Int] = 0 #:: 0 #:: 1 #:: tribs.zip(tribs.tail.zip(tribs.tail.tail)).map {
+    n =>
+      n._1 + n._2._1 + n._2._2
+  }
 }
