ec95989fe9f54957666eb7e9f857a7642f6498f8
shun <kaifenan@icloud.com>
2019-06-01 04:43:25 +0900

    practice

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
 object Main {
-  val tribs: Stream[Int] = ???
+  val tribs: Stream[Int] = 0 #:: 0 #:: 1 #:: tribs.zip(tribs.tail).zip(tribs.tail.tail).map{
+    n => n._1._1 + n._1._2 + n._2
+  }
 }
