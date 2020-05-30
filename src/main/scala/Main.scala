4615ca9213c9bc96be626dccaf2e8f30ff469408
Matsuoka Nao <58981974+matsuokanao@users.noreply.github.com>
2020-05-30 15:32:47 +0900

    トリボナッチ数列

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,4 @@
 object Main {
-  val tribs: Stream[Int] = ???
+  List(0, 0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274, 504, 927, 1705, 3136, 5768, 10609, 19513, 35890) 
+  val tribs: Stream[Int] = 0 #:: 0 #:: 1 #:: tribs.zip(tribs.tail).zip(tribs.tail.tail).map { n => n._1._1 + n._1._2 + n._2 }
 }
