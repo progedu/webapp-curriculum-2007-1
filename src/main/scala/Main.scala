3875d95a5e333a43b014b08fbe8a30488f8779c5
JojiKoike <joji.koike@gmail.com>
2020-06-13 17:30:10 +0900

    トリボナッチ数列を出力するStreamを実装。2.13系向けの実装例も追記

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,18 @@
-object Main {
-  val tribs: Stream[Int] = ???
+object Main extends App {
+  val tribs: Stream[Int] = {
+    0 #:: 0 #:: 1 #:: tribs.zip(tribs.tail).zip(tribs.tail.tail).map { n =>
+      n._1._1 + n._1._2 + n._2
+    }
+  }
+
+  // Scala-2.13系ではStream[T]はDeprecatedなので、代わりにLazyVal[T]を使う
+  // 以下、Scala-2.13系向け実装
+  /*
+  val tribs_213: LazyList[Int] = 0 #:: 0 #:: 1 #:: tribs_213
+    .zip(tribs_213.tail)
+    .zip(tribs_213.tail.tail)
+    .map { n =>
+      n._1._1 + n._1._2 + n._2
+    }
+ */
 }
